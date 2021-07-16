48603942476dc639c67ee6fc220d5c9fd91280c6
prosceel <ithesdv@gmail.com>
2021-07-16 22:14:47 +0300

    finished about-us section and services

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
-alert('Hello world');
+/*const header = document.querySelector('.header');
+
+const scrollHeader = () => {
+    if(window.scrollY > 50) {
+        header.classList.add('header--is-active');
+    }
+};
+
+window.onscroll = scrollHeader();*/

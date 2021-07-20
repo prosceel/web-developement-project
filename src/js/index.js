0f8f408b4dbf267ae5a4ee288aef1585e34891e2
prosceel <ithesdv@gmail.com>
2021-07-20 21:16:56 +0300

    finished pricing plan

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,15 @@
 import detectScrollbar from './detect-scrollbar'
 import resizeWindowHeight from './resize-window-height'
+import simpleParallax from 'simple-parallax-js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    resizeWindowHeight()
-    detectScrollbar()
-  })
+  resizeWindowHeight()
+  detectScrollbar()
+});
+
+const image = document.querySelectorAll('.home__image');
+new simpleParallax(image, {
+  scale: 1.3,
+  delay: .6,
+  transition: 'cubic-bezier(0,0,0,1)'
+});

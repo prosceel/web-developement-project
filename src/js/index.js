f280f8fa366ed8a73c3b5869e1978a115153a5a3
prosceel <ithesdv@gmail.com>
2021-07-21 20:34:00 +0300

    finished project

--- src/js/index.js
+++ src/js/index.js
@@ -7,8 +7,8 @@ document.addEventListener('DOMContentLoaded', () => {
   detectScrollbar()
 });
 
-const image = document.querySelectorAll('.home__image');
-new simpleParallax(image, {
+const home = document.querySelectorAll('.home__image');
+new simpleParallax(home, {
   scale: 1.3,
   delay: .6,
   transition: 'cubic-bezier(0,0,0,1)'

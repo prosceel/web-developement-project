3706377897243c17826e122dec813f123bf04e26
prosceel <ithesdv@gmail.com>
2021-07-21 22:46:57 +0300

    finished header-mobile

--- src/js/index.js
+++ src/js/index.js
@@ -13,3 +13,14 @@ new simpleParallax(home, {
   delay: .6,
   transition: 'cubic-bezier(0,0,0,1)'
 });
+
+const btnBurger = document.querySelector('.mobile-header__navigation-burger');
+const menu = document.querySelector('.mobile-header__menu');
+const html = document.querySelector('html');
+
+const openMenu = () => {
+  btnBurger.classList.toggle('mobile-header__navigation-burger--is-active');
+  menu.classList.toggle('mobile-header__menu--is-active');
+};
+
+btnBurger.addEventListener('click', openMenu);
